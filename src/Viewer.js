9f914f0776396621016b04f36b60ff04979e23e7
yunzi <yoonji5809@gmail.com>
2021-03-08 17:45:41 +0900

    구글 뷰어 팬닝 panning 기능 구현

--- src/Viewer.js
+++ src/Viewer.js
@@ -105,7 +105,127 @@ const modelPath4 = '/wp-content/uploads/test/BLA12_3_100k.gltf';
 
 function OnReset(){
     model.current.environmentImage ='neutral';
-    // model.current.maxFieldOfView="180";
+    //최대확대값
+    model.current.maxFieldOfView="180deg";
+    //최대축소값
+    model.current.minFieldOfView="0deg";
+
+    (() => {      
+      const tapDistance = 2;
+      let panning = false;
+      let panX, panY;
+      let startX, startY;
+      let lastX, lastY;
+      let metersPerPixel;
+    
+      const startPan = () => {
+        const orbit = model.current.getCameraOrbit();
+        const {theta, phi, radius} = orbit;
+        const psi = theta - model.current.turntableRotation;
+        metersPerPixel = 0.75 * radius / model.current.getBoundingClientRect().height;
+        panX = [-Math.cos(psi), 0, Math.sin(psi)];
+        panY = [
+          -Math.cos(phi) * Math.sin(psi),
+          Math.sin(phi),
+          -Math.cos(phi) * Math.cos(psi)
+        ];
+        model.current.interactionPrompt = 'none';
+      };
+    
+      const movePan = (thisX, thisY) => {
+        const dx = (thisX - lastX) * metersPerPixel;
+        const dy = (thisY - lastY) * metersPerPixel;
+        lastX = thisX;
+        lastY = thisY;
+    
+        const target = model.current.getCameraTarget();
+        target.x += dx * panX[0] + dy * panY[0];
+        target.y += dx * panX[1] + dy * panY[1];
+        target.z += dx * panX[2] + dy * panY[2];
+        model.current.cameraTarget = `${target.x}m ${target.y}m ${target.z}m`;
+    
+        // This pauses turntable rotation
+        model.current.dispatchEvent(new CustomEvent(
+              'camera-change', {detail: {source: 'user-interaction'}}));
+      };
+    
+      const recenter = (pointer) => {
+        panning = false;
+        if (Math.abs(pointer.clientX - startX) > tapDistance ||
+            Math.abs(pointer.clientY - startY) > tapDistance)
+          return;
+        const rect = model.current.getBoundingClientRect();
+        const x =  window.event.clientX - rect.left;
+        const y =  window.event.clientY - rect.top;
+        const hit = model.current.positionAndNormalFromPoint(x, y);
+        model.current.cameraTarget =
+            hit == null ? 'auto auto auto' : hit.position.toString();
+      };
+    
+      const onPointerUp = (event) => {
+        const pointer = event.clientX ? event : event.changedTouches[0];
+        if (Math.abs(pointer.clientX - startX) < tapDistance &&
+            Math.abs(pointer.clientY - startY) < tapDistance) {
+          recenter(pointer);
+        }
+        panning = false;
+      };
+    
+      model.current.addEventListener('mousedown', (event) => {
+        startX = event.clientX;
+        startY = event.clientY;
+        panning = event.button === 2 || event.ctrlKey || event.metaKey ||
+            event.shiftKey;
+        if (!panning)
+          return;
+    
+        lastX = startX;
+        lastY = startY;
+        startPan();
+        event.stopPropagation();
+      }, true);
+    
+      model.current.addEventListener('touchstart', (event) => {
+        startX = event.touches[0].clientX;
+        startY = event.touches[0].clientY;
+        panning = event.touches.length === 2;
+        if (!panning)
+          return;
+    
+        const {touches} = event;
+        lastX = 0.5 * (touches[0].clientX + touches[1].clientX);
+        lastY = 0.5 * (touches[0].clientY + touches[1].clientY);
+        startPan();
+      }, true);
+    
+      model.current.addEventListener('mousemove', (event) => {
+        if (!panning)
+          return;
+    
+        movePan(event.clientX, event.clientY);
+        event.stopPropagation();
+      }, true);
+    
+      model.current.addEventListener('touchmove', (event) => {
+        if (!panning || event.touches.length !== 2)
+          return;
+    
+        const {touches} = event;
+        const thisX = 0.5 * (touches[0].clientX + touches[1].clientX);
+        const thisY = 0.5 * (touches[0].clientY + touches[1].clientY);
+        movePan(thisX, thisY);
+      }, true);
+    
+      window.self.addEventListener('mouseup', (event) => {
+        recenter(event);
+      }, true);
+      
+      window.self.addEventListener('touchend', (event) => {
+        if (event.touches.length === 0) {
+          recenter(event.changedTouches[0]);
+        }
+      }, true);
+    })();
     
     //5초마다 배열에 넣은 각도값으로 모델 시점이 변경됨.
     // setInterval(() => {
